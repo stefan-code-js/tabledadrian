3e7136c3c07ce46c0f189acfbfaae3ddb643369c
Adrian Stefan <77865941+stefan-code-js@users.noreply.github.com>
2025-09-05 10:05:45 +0200

    Delete src directory

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Simple robots.txt generator. Allow all crawling and reference sitemap.
- */
-export async function GET() {
-  const body = `User-agent: *\nAllow: /\nSitemap: https://www.tabledadrian.com/sitemap.xml`;
-  return new Response(body, {
-    headers: { 'Content-Type': 'text/plain' },
-  });
-}
